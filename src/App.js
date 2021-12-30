31323bcdc7f2ed2d2b9f278bc21cced86c74768d
juliusBaranauskas <juliu100@gmail.com>
2021-12-30 20:31:22 +0200

    clean up toolbox

--- src/App.js
+++ src/App.js
@@ -31,36 +31,6 @@ export default function App() {
       },
       {
         kind: "category",
-        name: "Logic",
-        colour: "#5C81A6",
-        contents: [
-          {
-            kind: "block",
-            type: "controls_if",
-          },
-          {
-            kind: "block",
-            type: "logic_compare",
-          },
-        ],
-      },
-      {
-        kind: "category",
-        name: "Math",
-        colour: "#5CA65C",
-        contents: [
-          {
-            kind: "block",
-            type: "math_round",
-          },
-          {
-            kind: "block",
-            type: "math_number",
-          },
-        ],
-      },
-      {
-        kind: "category",
         name: "Custom",
         colour: "#5CA699",
         contents: [
